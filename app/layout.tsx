f19ec75e6469325d981e45b6eb48d6e105f7ee3f
José Dourado <jpdourado0210@gmail.com>
2026-03-25 10:03:04 +0000

    Atualiza favicon no metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,14 @@ export const metadata: Metadata = {
   title: 'Pokemon Adventure',
   description: 'Pokemon Adventure',
   generator: 'v0.dev',
+  icons: {
+    icon: [
+      { url: '/icon-light-32x32.png', media: '(prefers-color-scheme: light)' },
+      { url: '/icon-dark-32x32.png', media: '(prefers-color-scheme: dark)' },
+      { url: '/icon.svg', type: 'image/svg+xml' },
+    ],
+    apple: '/apple-icon.png',
+  },
 }
 
 export default function RootLayout({
